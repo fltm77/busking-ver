3ec00dcf56c1e88d6a457b3e6aa05dcba4e60cff
fltm77 <50696198+fltm77@users.noreply.github.com>
2020-08-21 14:32:31 +0900

    upd

---
+++ bus_king_erp_updated/src/kr/kosta/bus/model/RouteDAO.java
@@ -0,0 +1,15 @@
+package kr.kosta.bus.model;
+
+import java.util.HashMap;
+import java.util.List;
+
+public interface RouteDAO {
+	public List<RouteDTO> routeList(HashMap map);
+	public void routeInsert(RouteDTO dto);
+	public void routeUpdate(RouteDTO dto);
+	public void routeDelete(String r_no);
+	public int getRouteCount();
+	public RouteDTO routeSelect(RouteDTO dto);
+	public int routealloc(String r_no);
+	public int rnoCheck(String r_no);
+}
